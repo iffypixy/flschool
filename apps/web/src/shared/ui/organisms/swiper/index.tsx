ddba5c7d4e65bef02e484dd9960abe6d78882dc7
Ansat Euler <iffypixy@gmail.com>
2024-05-19 23:55:56 +0200

    feat: add swiper

---
+++ apps/web/src/shared/ui/organisms/swiper/index.tsx
@@ -0,0 +1,30 @@
+import Slider from "react-slick";
+import {cx} from "class-variance-authority";
+
+import {Icon} from "@shared/ui/icons";
+
+import "./index.css";
+
+const ArrowLeft: React.FC = () => (
+	<button className="min-w-[6rem] h-[6rem] rounded-full bg-gradient flex items-center justify-center">
+		<Icon.Chevron.Left className="fill-white w-[1.2rem] h-auto" />
+	</button>
+);
+
+const ArrowRight: React.FC = () => (
+	<button className="min-w-[6rem] h-[6rem] rounded-full bg-gradient flex items-center justify-center">
+		<Icon.Chevron.Right className="fill-white w-[1.2rem] h-auto" />
+	</button>
+);
+
+export const Swiper: React.FC<React.ComponentProps<typeof Slider>> = ({
+	className,
+	...props
+}) => (
+	<Slider
+		className={cx("flex items-center overflow-hidden", className)}
+		prevArrow={<ArrowLeft />}
+		nextArrow={<ArrowRight />}
+		{...props}
+	/>
+);
