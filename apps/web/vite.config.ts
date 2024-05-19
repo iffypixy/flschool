7d6a430c8214a6b012032aa971ed3710ee56cdd2
Ansat Euler <iffypixy@gmail.com>
2024-05-19 23:54:02 +0200

    feat: add @fonts/ alias for css

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,8 +2,14 @@ import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths(), svgr()],
+	resolve: {
+		alias: {
+			"@fonts": path.resolve(__dirname, "./src/shared/assets/fonts"),
+		},
+	},
 });
