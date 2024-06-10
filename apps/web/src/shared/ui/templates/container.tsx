b268cbed2ffa01aa5c9711dd6d26b689c00f181b
Ansat Euler <iffypixy@gmail.com>
2024-06-11 01:53:20 +0200

    refactor: improve container

--- apps/web/src/shared/ui/templates/container.tsx
+++ apps/web/src/shared/ui/templates/container.tsx
@@ -1,3 +1,5 @@
 export const Container: React.FCWC = ({children}) => (
-	<div className="max-w-[1280px] w-[90%] mx-auto">{children}</div>
+	<div className="max-w-[1280px] w-[90%] mx-auto md:w-full md:px-32 sm:px-24 xs:px-16">
+		{children}
+	</div>
 );
