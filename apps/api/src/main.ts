d1e1358d8148b1089bfa95b6810ff440c43465e2
Ansat Euler <iffypixy@gmail.com>
2024-06-11 17:30:56 +0200

    feat: integrate config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,9 +3,13 @@ import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {
+		cors: {
+			origin: process.env.CLIENT_ORIGIN.split(", "),
+		},
+	});
 
-	await app.listen(3000);
+	await app.listen(+process.env.PORT);
 }
 
 bootstrap();
